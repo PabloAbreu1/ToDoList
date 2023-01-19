cbba853f4b34fce6d95a6cec7a3584c9ead3ba0c
PabloAbreu1 <pagustin2404@gmail.com>
2023-01-19 13:30:35 +0000

    cmabio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+	const[lista,setLista]=useState([])
+	const[seved,setSeved]=useState("")
+
+	// function handlelista(e) {
+	// 	console.log(e.target.value)
+	// 	setlista(e.target.value)
+	// }
+
+	const guardarTarea=(e)=>{
+		e.preventDefault()
+		setSeved("")
+		setLista([...lista, { id: lista.length, name: seved }])
+	}
+
+console.log(lista)
+console.log(seved)
+
+const borrar =(id)=>{
+setLista(lista.filter((item) => item.id !== id));
+
+	
+}
+
+return (
+<>
+<h1 className="d-flex justify-content-center">ToDoList</h1>
+<form className="container" onSubmit={guardarTarea}>
+<div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label"></label>
+    {/*2. definimos el evento ochange en el input */}
+	<input 
+	type="text" 
+	className="form-control" 
+	id="exampleInputEmail1" 
+	aria-describedby="emailHelp" 
+	onChange={(e)=>{setSeved(e.target.value)}}/>
+</div>
+
+  <ul className="list-group">
+  {lista.map((item)=>(<li className="list-group-item" key={item.id}>{item.name}<button type="button" className="btn btn-outline-light" onClick={(()=>borrar(item.id))}>X</button></li>))}
+
+  </ul>
+  
+
+</form>
+
+</>
 	);
 };
 
